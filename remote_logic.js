312325174b931d46e4b7f986507a3ecf97b8cb2a
Isysprey@foxmail.com <58359338+badApple001@users.noreply.github.com>
2025-09-06 15:15:11 +0800

    Create remote_logic.js

---
+++ remote_logic.js
@@ -0,0 +1,9 @@
+// remote_logic.js
+({
+    tick: function () {
+        let dialog = document.querySelector('.dilog');
+        if (dialog) {
+            console.log("[远程逻辑] 检测到了 .dilog 弹窗");
+        }
+    }
+})
